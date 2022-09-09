593d17892f8cf9460726f5b814aa30c9c47764b3
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-09-09 13:33:29 -0300

    trabajando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogosComponent } from './componentes/logos/logos.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercademiComponent } from './componentes/acercademi/acercademi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogosComponent
+    LogosComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercademiComponent
   ],
   imports: [
     BrowserModule,
