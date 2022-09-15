eb4bad922681f81f6b8692e3311b270bd06186ac
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-09-15 09:52:48 -0300

    bueno
    seguimos

---
+++ backEnd/src/main/java/com/proyectofinal/lpc/LpcApplication.java
@@ -0,0 +1,14 @@
+package com.proyectofinal.lpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpcApplication.class, args);
+                System.out.println("Hii");
+	}
+
+}
---
+++ backEnd/src/test/java/com/proyectofinal/lpc/LpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.lpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
